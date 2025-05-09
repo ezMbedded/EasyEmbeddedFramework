1fc0e4c1369a1e8ec9f0e441e1ca7e4d0ad350ff
Hai Nguyen <hainguyen.eeit@gmail.com>
2025-05-10 00:12:09 +0200

    first implemenation of driver

--- easy_embedded/hal/gpio/ez_gpio.c
+++ easy_embedded/hal/gpio/ez_gpio.c
@@ -25,13 +25,13 @@
 *****************************************************************************/
 #include "ez_gpio.h"
 
-#if (EZ_GPIO_ENABLE == 1)
+#if (EZ_GPIO == 1)
 
 #define DEBUG_LVL   LVL_TRACE   /**< logging level */
 #define MOD_NAME    "ez_gpio"       /**< module name */
 #include "ez_logging.h"
 
-/*the rest of include go here*/
+#include <string.h>
 
 /*****************************************************************************
 * Component Preprocessor Macros
@@ -41,12 +41,15 @@
 /*****************************************************************************
 * Component Typedefs
 *****************************************************************************/
-/* None */
 
 /*****************************************************************************
 * Component Variable Definitions
 *****************************************************************************/
-/* None */
+static struct Node hw_driver_list = EZ_LINKEDLIST_INIT_NODE(hw_driver_list);
+/**< List of HW driver implementation  */
+
+static struct Node instance_list = EZ_LINKEDLIST_INIT_NODE(instance_list);
+/**< Keep tracks of instance register to this driver */
 
 /*****************************************************************************
 * Function Definitions
@@ -56,14 +59,163 @@
 /*****************************************************************************
 * Public functions
 *****************************************************************************/
-int sum(int a, int b)
-{
-    return a + b;
-}
+
 
 /*****************************************************************************
 * Local functions
 *****************************************************************************/
+EZ_DRV_STATUS ezGpio_SystemRegisterHwDriver(struct ezGpioDriver *hw_gpio_driver)
+{
+    EZ_DRV_STATUS status = STATUS_ERR_GENERIC;
 
-#endif /* EZ_GPIO_ENABLE == 1 */
+    EZTRACE("ezGpio_SystemRegisterHwDriver()");
+    if(hw_gpio_driver == NULL)
+    {
+        status = STATUS_ERR_ARG;
+        EZERROR("hw_gpio_driver == NULL");
+    }
+    else
+    {
+        EZ_LINKEDLIST_ADD_TAIL(&hw_driver_list, &hw_gpio_driver->ll_node);
+        status = ezEventNotifier_CreateSubject(&hw_gpio_driver->gpio_event);;
+    }
+
+    return status;
+}
+
+
+EZ_DRV_STATUS ezGpio_SystemUnregisterHwDriver(struct ezGpioDriver *hw_gpio_driver)
+{
+        EZ_DRV_STATUS status = STATUS_ERR_GENERIC;
+
+    EZTRACE("ezUart_SystemUnregisterHwDriver()");
+    if(hw_gpio_driver == NULL)
+    {
+        status = STATUS_ERR_ARG;
+        EZERROR("hw_gpio_driver == NULL");
+    }
+    else
+    {
+        EZ_LINKEDLIST_UNLINK_NODE(&hw_gpio_driver->ll_node);
+        status = STATUS_OK;
+    }
+
+    return status;
+}
+
+EZ_DRV_STATUS ezGpio_RegisterInstance(ezGpioDrvInstance_t *inst,
+                                      const char *driver_name,
+                                      ezObserver *event_subcriber,
+                                      EVENT_CALLBACK callback)
+{
+    EZ_DRV_STATUS status = STATUS_ERR_DRV_NOT_FOUND;
+    struct Node* it_node = NULL;
+    struct ezGpioDriver *gpio_drv = NULL;
+
+    EZTRACE("ezGpio_RegisterInstance(name = %s)", driver_name);
+    if((inst == NULL) || (driver_name == NULL))
+    {
+        status = STATUS_ERR_ARG;
+    }
+    else
+    {
+        EZ_LINKEDLIST_FOR_EACH(it_node, &hw_driver_list)
+        {
+            gpio_drv = EZ_LINKEDLIST_GET_PARENT_OF(it_node, ll_node, struct ezGpioDriver);
+            if(strcmp(gpio_drv->common.name, driver_name) == 0)
+            {
+                EZDEBUG("Found driver!");
+                inst->driver = (void*)gpio_drv;
+                inst->calback = NULL;
+                if(ezEventNotifier_CreateObserver(event_subcriber, callback) != ezSUCCESS)
+                {
+                    EZERROR("Cannot create observer");
+                    status = STATUS_ERR_GENERIC;
+                }
+                else
+                {
+                    ezEventNotifier_SubscribeToSubject(&gpio_drv->gpio_event, event_subcriber);
+                    status = STATUS_OK;
+                }
+                break;
+            }
+        }
+    }
+
+    return status;
+}
+
+
+EZ_DRV_STATUS ezUart_UnregisterInstance(ezGpioDrvInstance_t *inst)
+{
+    EZ_DRV_STATUS status = STATUS_ERR_GENERIC;
+
+    EZTRACE("ezGpio_UnregisterInstance()");
+    if(inst == NULL)
+    {
+        status = STATUS_ERR_ARG;
+    }
+    else
+    {
+        inst->driver = NULL;
+        EZDEBUG("unregister success");
+        status = STATUS_OK;
+    }
+
+    return status;
+}
+
+
+EZ_GPIO_PIN_STATE ezGpio_ReadPin(ezGpioDrvInstance_t *inst, uint16_t port_index, uint16_t pin_index)
+{
+    EZ_GPIO_PIN_STATE state = EZ_GPIO_PIN_ERROR;
+    struct ezGpioDriver *drv = NULL;
+
+    EZTRACE("ezGpio_ReadPin()");
+    drv = (struct ezGpioDriver*)ezDriver_GetDriverFromInstance(inst);
+    if(drv != NULL)
+    {
+        EZTRACE("Found driver");
+        if(ezDriver_IsDriverAvailable(inst, &drv->common) == true)
+        {
+            EZTRACE("Driver = %s is available", drv->common.name);
+            ezDriver_LockDriver(inst, &drv->common);
+            if(drv->interface.read_pin)
+            {
+                state = drv->interface.read_pin(port_index, pin_index);
+            }
+            ezDriver_UnlockDriver(&drv->common);
+        }
+    }
+    return state;
+}
+
+EZ_DRV_STATUS ezGpio_WritePin(ezGpioDrvInstance_t *inst, uint16_t port_index, uint16_t pin_index, EZ_GPIO_PIN_STATE state)
+{
+    EZ_DRV_STATUS status = STATUS_ERR_DRV_NOT_FOUND;
+    struct ezGpioDriver *drv = NULL;
+
+    EZTRACE("ezUart_AsyncTransmit()");
+    drv = (struct ezGpioDriver*)ezDriver_GetDriverFromInstance(inst);
+    if(drv != NULL)
+    {
+        EZTRACE("Found driver");
+        status = STATUS_BUSY;
+        if(ezDriver_IsDriverAvailable(inst, &drv->common) == true)
+        {
+            EZTRACE("Driver = %s is available", drv->common.name);
+            status = STATUS_ERR_INF_NOT_EXIST;
+            ezDriver_LockDriver(inst, &drv->common);
+            if(drv->interface.write_pin)
+            {
+                status = drv->interface.write_pin(port_index, pin_index, state);
+            }
+            ezDriver_UnlockDriver(&drv->common);
+        }
+    }
+    return status;
+}
+
+
+#endif /* EZ_GPIO == 1 */
 /* End of file*/
--- easy_embedded/hal/gpio/ez_gpio.h
+++ easy_embedded/hal/gpio/ez_gpio.h
@@ -34,7 +34,12 @@ extern "C" {
 #include "ez_target_config.h"
 #endif
 
-#if (EZ_GPIO_ENABLE == 1)
+#if (EZ_GPIO == 1)
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "ez_driver_def.h"
+#include "ez_event_notifier.h"
 
 /*****************************************************************************
 * Component Preprocessor Macros
@@ -44,15 +49,67 @@ extern "C" {
 /*****************************************************************************
 * Component Typedefs
 *****************************************************************************/
-
-/** @brief definition of a new type
- *
- */
-typedef struct
+/** @brief GPIO mode */
+typedef enum
 {
-    int a;  /**< an integer */
-    int b;  /**< an integer */
-}aType;
+    EZ_GPIO_MODE_INPUT,    /**< input */
+    EZ_GPIO_MODE_OUTPUT,   /**< output */
+}EZ_GPIO_MODE;
+
+
+/** @brief GPIO state */
+typedef enum
+{
+    EZ_GPIO_PIN_LOW,    /**< low */
+    EZ_GPIO_PIN_HIGH,   /**< high */
+    EZ_GPIO_PIN_ERROR,  /**< error */
+}EZ_GPIO_PIN_STATE;
+
+
+typedef enum
+{
+    EZ_GPIO_PULL_UP,   /**< pull up */
+    EZ_GPIO_PULL_DOWN, /**< pull down */
+    EZ_GPIO_NO_PULL,   /**< no pull */
+}EZ_GPIO_PULL;
+
+/****************************************************************************/
+/* List of API implemented by the HW driver. Users are not supposed to use
+ * these API.
+ */
+
+/** @brief Read state of a pin
+ *  @param[in]  port_index: index of the GPIO port
+ *  @param[in]  pin_index: index of the GPIO pin
+ *  @return     EZ_GPIO_PIN_LOW or EZ_GPIO_PIN_HIGH
+ */
+typedef EZ_GPIO_PIN_STATE(*ezHwGpio_ReadPin)(uint16_t port_index, uint16_t pin_index);
+
+/** @brief Write state of a pin
+ *  @param[in]  port_index: index of the GPIO port
+ *  @param[in]  pin_index: index of the GPIO pin
+ *  @param[in]  state: state of the pin
+ *  @return     EZ_DRV_STATUS
+ */
+typedef EZ_DRV_STATUS(*ezHwGpio_WritePin)(uint16_t port_index, uint16_t pin_index, EZ_GPIO_PIN_STATE state);
+
+struct ezHwGpioInterface
+{
+    ezHwGpio_ReadPin   read_pin;   /**< Read pin */
+    ezHwGpio_WritePin  write_pin;  /**< Write pin */
+};
+
+struct ezGpioDriver
+{
+    struct Node                 ll_node;    /* linked list node to link to list of hw driver implmentation */
+    struct ezDriverCommon       common;     /* Common data of driver */
+    struct ezHwGpioInterface    interface;  /* HW API */
+    ezSubject                   gpio_event; /* Subject for event notification */
+};
+
+/** @brief Define Uart Driver Instance
+ */
+typedef struct ezDrvInstance ezGpioDrvInstance_t;
 
 
 /*****************************************************************************
@@ -64,31 +121,23 @@ typedef struct
 * Function Prototypes
 *****************************************************************************/
 
-/*****************************************************************************
-* Function: sum
-*//** 
-* @brief one line description
-*
-* @details Detail description
-*
-* @param    a: (IN)pointer to the ring buffer
-* @param    b: (IN)size of the ring buffer
-* @return   None
-*
-* @pre None
-* @post None
-*
-* \b Example
-* @code
-* sum(a, b);
-* @endcode
-*
-* @see sum
-*
-*****************************************************************************/
-int sum(int a, int b);
+EZ_DRV_STATUS ezGpio_SystemRegisterHwDriver(struct ezGpioDriver *hw_gpio_driver);
 
-#endif /* EZ_GPIO_ENABLE == 1 */
+EZ_DRV_STATUS ezGpio_SystemUnregisterHwDriver(struct ezGpioDriver *hw_gpio_driver);
+
+EZ_DRV_STATUS ezGpio_RegisterInstance(ezGpioDrvInstance_t *inst,
+                                      const char *driver_name,
+                                      ezObserver *event_subcriber,
+                                      EVENT_CALLBACK callback);
+
+EZ_DRV_STATUS ezUart_UnregisterInstance(ezGpioDrvInstance_t *inst);
+
+EZ_GPIO_PIN_STATE ezGpio_ReadPin(ezGpioDrvInstance_t *inst, uint16_t port_index, uint16_t pin_index);
+
+EZ_DRV_STATUS ezGpio_WritePin(ezGpioDrvInstance_t *inst, uint16_t port_index, uint16_t pin_index, EZ_GPIO_PIN_STATE state);
+
+
+#endif /* EZ_GPIO == 1 */
 
 #ifdef __cplusplus
 }
