3c3615c5beda842bf53eb527f6ccb787215743e5
Hai Nguyen <hainguyen.eeit@gmail.com>
2025-05-25 12:08:55 +0200

    First implementation for i2c

---
+++ easy_embedded/hal/i2c/ez_i2c.c
@@ -0,0 +1,294 @@
+/*****************************************************************************
+* Filename:         ez_i2c.c
+* Author:           Hai Nguyen
+* Original Date:    25.05.2025
+*
+* ----------------------------------------------------------------------------
+* Contact:          Hai Nguyen
+*                   hainguyen.eeit@gmail.com
+*
+* ----------------------------------------------------------------------------
+* License: This file is published under the license described in LICENSE.md
+*
+*****************************************************************************/
+
+/** @file   ez_i2c.c
+ *  @author Hai Nguyen
+ *  @date   25.05.2025
+ *  @brief  One line description of the component
+ *
+ *  @details Detail description of the component
+ */
+
+/*****************************************************************************
+* Includes
+*****************************************************************************/
+#include "ez_i2c.h"
+
+#if (EZ_I2C == 1)
+
+#define DEBUG_LVL   LVL_TRACE   /**< logging level */
+#define MOD_NAME    "ez_i2c"       /**< module name */
+#include "ez_logging.h"
+
+#include <string.h>
+
+/*****************************************************************************
+* Component Preprocessor Macros
+*****************************************************************************/
+/* None */
+
+/*****************************************************************************
+* Component Typedefs
+*****************************************************************************/
+static struct Node hw_driver_list = EZ_LINKEDLIST_INIT_NODE(hw_driver_list);
+
+/*****************************************************************************
+* Component Variable Definitions
+*****************************************************************************/
+/* None */
+
+/*****************************************************************************
+* Function Definitions
+*****************************************************************************/
+/* None */
+
+/*****************************************************************************
+* Public functions
+*****************************************************************************/
+EZ_DRV_STATUS ezI2c_SystemRegisterHwDriver(struct ezI2cDriver *hw_driver)
+{
+    EZTRACE("ezI2c_SystemRegisterHwDriver()");
+    if(hw_driver == NULL)
+    {
+        EZERROR("hw_driver == NULL");
+        return STATUS_ERR_ARG;
+    }
+
+    hw_driver->initialized = false;
+    EZ_LINKEDLIST_ADD_TAIL(&hw_driver_list, &hw_driver->ll_node);
+
+    if(ezEventNotifier_CreateSubject(&hw_driver->i2c_event) == ezSUCCESS)
+    {
+        EZDEBUG("Register OK");
+        return STATUS_OK;
+    }
+    else
+    {
+        EZERROR("Cannot create subject for GPIO driver %s", hw_driver->common.name);
+        return STATUS_ERR_GENERIC;
+    }
+    return STATUS_ERR_GENERIC;
+}
+
+EZ_DRV_STATUS ezI2c_SystemUnregisterHwDriver(struct ezI2cDriver *hw_driver)
+{
+    EZTRACE("ezI2c_SystemUnregisterHwDriver()");
+    if(hw_driver == NULL)
+    {
+        EZERROR("hw_driver == NULL");
+        return STATUS_ERR_ARG;
+    }
+    hw_driver->initialized = false;
+    EZ_LINKEDLIST_UNLINK_NODE(&hw_driver->ll_node);
+    return STATUS_OK;
+}
+
+EZ_DRV_STATUS ezI2c_RegisterInstance(ezI2cDrvInstance_t *inst,
+                                     const char *driver_name,
+                                     EVENT_CALLBACK callback)
+{
+    struct Node* it_node = NULL;
+    struct ezI2cDriver *i2c_drv = NULL;
+
+    EZTRACE("ezI2c_RegisterInstance(name = %s)", driver_name);
+    if((inst == NULL) || (driver_name == NULL))
+    {
+        return STATUS_ERR_ARG;
+    }
+
+    EZ_LINKEDLIST_FOR_EACH(it_node, &hw_driver_list)
+    {
+        i2c_drv = EZ_LINKEDLIST_GET_PARENT_OF(it_node, ll_node, struct ezI2cDriver);
+        if(strcmp(i2c_drv->common.name, driver_name) == 0)
+        {
+            EZDEBUG("Found driver!");
+            inst->drv_instance.driver = (void*)i2c_drv;
+            inst->drv_instance.calback = NULL;
+
+            if(ezEventNotifier_CreateObserver(&inst->event_subcriber, callback) != ezSUCCESS)
+            {
+                EZERROR("Cannot create observer");
+                return STATUS_ERR_GENERIC;
+            }
+
+            if(ezEventNotifier_SubscribeToSubject(&i2c_drv->i2c_event, &inst->event_subcriber) != ezSUCCESS)
+            {
+                EZERROR("Cannot subscribe to subject");
+                return STATUS_ERR_GENERIC;
+            }
+
+            return STATUS_OK;
+        }
+    }
+
+    return STATUS_ERR_DRV_NOT_FOUND;
+}
+
+EZ_DRV_STATUS ezI2c_UnregisterInstance(ezI2cDrvInstance_t *inst)
+{
+    EZTRACE("ezI2c_UnregisterInstance()");
+    if(inst == NULL)
+    {
+        return STATUS_ERR_ARG;
+    }
+    inst->drv_instance.driver = NULL;
+    EZDEBUG("unregister success");
+    return STATUS_OK;
+}
+
+EZ_DRV_STATUS ezI2c_Initialize(ezI2cDrvInstance_t *inst, ezI2cConfig_t *config)
+{
+    EZTRACE("ezI2c_Initialize()");
+    EZ_DRV_STATUS status = STATUS_ERR_GENERIC;
+    struct ezI2cDriver *drv = (struct ezI2cDriver*)ezDriver_GetDriverFromInstance((struct ezDrvInstance*)inst);
+
+    if(drv == NULL)
+    {
+        EZERROR("Driver not found");
+        return STATUS_ERR_DRV_NOT_FOUND;
+    }
+
+    if(ezDriver_IsDriverAvailable((struct ezDrvInstance*)inst, &drv->common) == false)
+    {
+        EZTRACE("Driver = %s is not available", drv->common.name);
+        return STATUS_ERR_INF_NOT_EXIST;
+    }
+
+    ezDriver_LockDriver((struct ezDrvInstance*)inst, &drv->common);
+    if(drv->interface.initialize != NULL)
+    {
+        status = drv->interface.initialize(config);
+    }
+    ezDriver_UnlockDriver(&drv->common);
+
+    if(status == STATUS_OK)
+    {
+        drv->initialized = true;
+        EZDEBUG("Driver %s initialized", drv->common.name);
+    }
+
+    return status;
+}
+
+EZ_DRV_STATUS ezI2c_TransmitSync(ezI2cDrvInstance_t *inst,
+                                 uint16_t address,
+                                 const uint8_t *data,
+                                 size_t length,
+                                 uint32_t timeout_millis)
+{
+    EZTRACE("ezI2c_TransmitSync()");
+    EZ_DRV_STATUS status = STATUS_ERR_DRV_NOT_FOUND;
+    struct ezI2cDriver *drv = (struct ezI2cDriver*)ezDriver_GetDriverFromInstance((struct ezDrvInstance*)inst);
+
+    if(drv != NULL)
+    {
+        status = STATUS_BUSY;
+        if(ezDriver_IsDriverAvailable((struct ezDrvInstance*)inst, &drv->common) == true)
+        {
+            status = STATUS_ERR_INF_NOT_EXIST;
+            ezDriver_LockDriver((struct ezDrvInstance*)inst, &drv->common);
+            if(drv->interface.transmit_sync)
+            {
+                status = drv->interface.transmit_sync(address, data, length, timeout_millis);
+            }
+            ezDriver_UnlockDriver(&drv->common);
+        }
+    }
+    return status;
+}
+
+EZ_DRV_STATUS ezI2c_TransmitAsync(ezI2cDrvInstance_t *inst,
+                                  uint16_t address,
+                                  const uint8_t *data,
+                                  size_t length)
+{
+    EZTRACE("ezI2c_TransmitAsync()");
+    EZ_DRV_STATUS status = STATUS_ERR_DRV_NOT_FOUND;
+    struct ezI2cDriver *drv = (struct ezI2cDriver*)ezDriver_GetDriverFromInstance((struct ezDrvInstance*)inst);
+
+    if(drv != NULL)
+    {
+        status = STATUS_BUSY;
+        if(ezDriver_IsDriverAvailable((struct ezDrvInstance*)inst, &drv->common) == true)
+        {
+            status = STATUS_ERR_INF_NOT_EXIST;
+            ezDriver_LockDriver((struct ezDrvInstance*)inst, &drv->common);
+            if(drv->interface.transmit_async)
+            {
+                status = drv->interface.transmit_async(address, data, length);
+            }
+            ezDriver_UnlockDriver(&drv->common);
+        }
+    }
+    return status;
+}
+
+EZ_DRV_STATUS ezI2c_ReceiveSync(ezI2cDrvInstance_t *inst,
+                                uint16_t address,
+                                uint8_t *data,
+                                size_t length,
+                                uint32_t timeout_millis)
+{
+    EZTRACE("ezI2c_ReceiveSync()");
+    EZ_DRV_STATUS status = STATUS_ERR_DRV_NOT_FOUND;
+    struct ezI2cDriver *drv = (struct ezI2cDriver*)ezDriver_GetDriverFromInstance((struct ezDrvInstance*)inst);
+
+    if(drv != NULL)
+    {
+        status = STATUS_BUSY;
+        if(ezDriver_IsDriverAvailable((struct ezDrvInstance*)inst, &drv->common) == true)
+        {
+            status = STATUS_ERR_INF_NOT_EXIST;
+            ezDriver_LockDriver((struct ezDrvInstance*)inst, &drv->common);
+            if(drv->interface.receive_sync)
+            {
+                status = drv->interface.receive_sync(address, data, length, timeout_millis);
+            }
+            ezDriver_UnlockDriver(&drv->common);
+        }
+    }
+    return status;
+}
+
+EZ_DRV_STATUS ezI2c_ReceiveAsync(ezI2cDrvInstance_t *inst,
+                                 uint16_t address,
+                                 uint8_t *data,
+                                 size_t length)
+{
+    EZTRACE("ezI2c_ReceiveAsync()");
+    EZ_DRV_STATUS status = STATUS_ERR_DRV_NOT_FOUND;
+    struct ezI2cDriver *drv = (struct ezI2cDriver*)ezDriver_GetDriverFromInstance((struct ezDrvInstance*)inst);
+
+    if(drv != NULL)
+    {
+        status = STATUS_BUSY;
+        if(ezDriver_IsDriverAvailable((struct ezDrvInstance*)inst, &drv->common) == true)
+        {
+            status = STATUS_ERR_INF_NOT_EXIST;
+            ezDriver_LockDriver((struct ezDrvInstance*)inst, &drv->common);
+            if(drv->interface.receive_async)
+            {
+                status = drv->interface.receive_async(address, data, length);
+            }
+            ezDriver_UnlockDriver(&drv->common);
+        }
+    }
+    return status;
+}
+/*****************************************************************************
+* Local functions
+*****************************************************************************/
+
+#endif /* EZ_I2C == 1 */
+/* End of file*/
---
+++ easy_embedded/hal/i2c/ez_i2c.h
@@ -0,0 +1,185 @@
+/*****************************************************************************
+* Filename:         ez_i2c.h
+* Author:           Hai Nguyen
+* Original Date:    25.05.2025
+*
+* ----------------------------------------------------------------------------
+* Contact:          Hai Nguyen
+*                   hainguyen.eeit@gmail.com
+*
+* ----------------------------------------------------------------------------
+* License: This file is published under the license described in LICENSE.md
+*
+*****************************************************************************/
+
+/** @file   ez_i2c.h
+ *  @author Hai Nguyen
+ *  @date   25.05.2025
+ *  @brief  One line description of the component
+ *
+ *  @details Detail description of the component
+ */
+
+#ifndef _EZ_I2C_H
+#define _EZ_I2C_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*****************************************************************************
+* Includes
+*****************************************************************************/
+#if (EZ_BUILD_WITH_CMAKE == 0U)
+#include "ez_target_config.h"
+#endif
+
+#if (EZ_I2C == 1)
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "ez_driver_def.h"
+#include "ez_event_notifier.h"
+
+/*****************************************************************************
+* Component Preprocessor Macros
+*****************************************************************************/
+#define A_MACRO     1   /**< a macro*/
+
+/*****************************************************************************
+* Component Typedefs
+*****************************************************************************/
+
+/**
+ * @enum
+ * @brief Enumeration for I2C related constants or states.
+ *
+ * This enum is used to define various constants or states relevant to the I2C hardware abstraction layer.
+ * The specific values and their descriptions should be provided below each enumerator.
+ */
+typedef enum
+{
+    EZ_I2C_MODE_MASTER,  /**< I2C master mode */
+    EZ_I2C_MODE_SLAVE,   /**< I2C slave mode */
+} EZ_I2C_MODE;
+
+
+typedef enum
+{
+    EZ_I2C_SPEED_STANDARD,  /**< Standard speed (100 kHz) */
+    EZ_I2C_SPEED_FAST,      /**< Fast speed (400 kHz) */
+    EZ_I2C_SPEED_HIGH,      /**< High speed (1 MHz+), depending on HW support */
+} EZ_I2C_SPEED;
+
+
+typedef enum
+{
+    EZ_I2C_ADDRESSING_MODE_7BIT,  /**< 7-bit addressing mode */
+    EZ_I2C_ADDRESSING_MODE_10BIT, /**< 10-bit addressing mode */
+} EZ_I2C_ADDRESSING_MODE;
+
+typedef struct
+{
+    EZ_I2C_MODE mode;                /**< I2C mode (master/slave) */
+    EZ_I2C_SPEED speed;              /**< I2C speed (standard/fast/high) */
+    EZ_I2C_ADDRESSING_MODE addressing_mode; /**< Addressing mode (7-bit/10-bit) */
+} ezI2cConfig_t;
+
+
+typedef EZ_DRV_STATUS (*ezHwI2c_Initialize)(ezI2cConfig_t *config);
+typedef EZ_DRV_STATUS (*ezHwI2c_TransmitSync)(uint16_t address,
+                                              const uint8_t *data,
+                                              size_t length,
+                                              uint32_t timeout_millis);
+
+typedef EZ_DRV_STATUS (*ezHwI2c_TransmitAsync)(uint16_t address,
+                                               const uint8_t *data,
+                                               size_t length);
+
+typedef EZ_DRV_STATUS (*ezHwI2c_ReceiveSync)(uint16_t address,
+                                             uint8_t *data,
+                                             size_t length,
+                                             uint32_t timeout_millis);
+
+typedef EZ_DRV_STATUS (*ezHwI2c_ReceiveAsync)(uint16_t address,
+                                              uint8_t *data,
+                                              size_t length);
+
+struct ezHwI2cInterface
+{
+    ezHwI2c_Initialize     initialize;      /**< Initialize I2C interface */
+    ezHwI2c_TransmitSync   transmit_sync;   /**< Transmit data synchronously */
+    ezHwI2c_TransmitAsync  transmit_async;  /**< Transmit data asynchronously */
+    ezHwI2c_ReceiveSync    receive_sync;    /**< Receive data synchronously */
+    ezHwI2c_ReceiveAsync   receive_async;   /**< Receive data asynchronously */
+};
+
+struct ezI2cDriver
+{
+    struct Node                 ll_node;    /* linked list node to link to list of hw driver implmentation */
+    struct ezDriverCommon       common;     /* Common data of driver */
+    struct ezHwI2cInterface     interface;  /* HW API */
+    ezSubject                   i2c_event;  /* Subject for event notification */
+    bool                        initialized; /* Flag to check if the driver is initialized */
+};
+
+
+typedef struct{
+    struct ezDrvInstance    drv_instance;    /**< Driver instance */
+    ezObserver              event_subcriber; /**< Pointer to the event subscriber */
+} ezI2cDrvInstance_t;
+
+/*****************************************************************************
+* Component Variable Definitions
+*****************************************************************************/
+/* None */
+
+/*****************************************************************************
+* Function Prototypes
+*****************************************************************************/
+
+EZ_DRV_STATUS ezI2c_SystemRegisterHwDriver(struct ezI2cDriver *hw_driver);
+
+EZ_DRV_STATUS ezI2c_SystemUnregisterHwDriver(struct ezI2cDriver *hw_driver);
+
+EZ_DRV_STATUS ezI2c_RegisterInstance(ezI2cDrvInstance_t *inst,
+                                      const char *driver_name,
+                                      EVENT_CALLBACK callback);
+
+EZ_DRV_STATUS ezI2c_UnregisterInstance(ezI2cDrvInstance_t *inst);
+
+EZ_DRV_STATUS ezI2c_Initialize(ezI2cDrvInstance_t *inst, ezI2cConfig_t *config);
+
+EZ_DRV_STATUS ezI2c_TransmitSync(ezI2cDrvInstance_t *inst,
+                                 uint16_t address,
+                                 const uint8_t *data,
+                                 size_t length,
+                                 uint32_t timeout_millis);
+
+EZ_DRV_STATUS ezI2c_TransmitAsync(ezI2cDrvInstance_t *inst,
+                                  uint16_t address,
+                                  const uint8_t *data,
+                                  size_t length);
+
+EZ_DRV_STATUS ezI2c_ReceiveSync(ezI2cDrvInstance_t *inst,
+                                uint16_t address,
+                                uint8_t *data,
+                                size_t length,
+                                uint32_t timeout_millis);
+
+EZ_DRV_STATUS ezI2c_ReceiveAsync(ezI2cDrvInstance_t *inst,
+                                 uint16_t address,
+                                 uint8_t *data,
+                                 size_t length);
+
+
+#endif /* EZ_I2C == 1 */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _EZ_I2C_H */
+
+
+/* End of file */
